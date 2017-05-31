f0da7427806121c2d28e7239da6922009d47fb66
Wenjie Zhang <zhang_a_a_a@outlook.com>
2017-05-31 23:28:30 +0800

    d

--- SubmitAdd.php
+++ SubmitAdd.php
@@ -2,42 +2,33 @@
 
 
 session_start();
-if(! $_SESSION["admin"] && ( $_SESSION["user"]!=$_POST["userId"] ) ){
+if(( $_SESSION["user"]!=$_POST["userId"] ) ){
 	echo "<script>alert('未登录'); window.location.href='Login.php';</script>";
 	header("location:Login.php");
 }
 
+require_once("mysqliconn.php");
+$stmt = $dbConnection->prepare("INSERT INTO `Submit` (`problemId`, `userId`, `submitTime`) VALUES (?, ?, CURRENT_TIMESTAMP)");
+$stmt->bind_param('ss', $_POST["problemId"], $_POST["userId"]);
 
-$q = sprintf("INSERT INTO `OJ`.`Submit` (`submitId`, `problemId`, `userId`, `submitTime`, `result`, `runTime`) VALUES ('', '%s', '%s', CURRENT_TIMESTAMP, '', '')", $_POST["problemId"], $_POST["userId"]);
-
-
-if ($_FILES["submitCode"]["error"] > 0){
+if ($_FILES["submitCode"]["error"] > 0) {
   	echo "<script>alert('上传错误'); window.location.href='index1.php';</script>";
-}
-else{
-
-
-		$link=mysql_connect('localhost:3306','root','phisics')or die("数据库连接失败");
-		mysql_select_db('OJ',$link);//选择数据库
-		mysql_query("set names utf8");//设置编码格式
-
-		$res = mysql_query($q);
-
-		if($res){
-			$getId=mysql_insert_id();
+} else {
+		if($stmt->execute()){
+			$getId=$dbConnection->insert_id;
 			if(!is_dir("dat/submissions/".$getId)){
 				mkdir("dat/submissions/".$getId);
 			}
-			move_uploaded_file($_FILES["submitCode"]["tmp_name"], "dat/submissions/".$getId."/src.cpp");
+			move_uploaded_file($_FILES["submitCode"]["tmp_name"], "dat/problems/".$getId."/src.cpp");
 			shell_exec("/opt/OJ/judge/sendjudge.py ".$getId);
 			echo "<script>alert('提交成功'); window.location.href='ShowStatus.php';</script>";
 		}
 		else{
-			echo "<script>alert('提交失败: 数据库错误'); window.location.href='index1.php';</script>";
+			echo "<script>alert('提交失败: 数据库错误'); window.location.href='ProblemManage.php';</script>";
 		}
-
     }
-
+$stmt->close();
+$dbConnection->close();
 
 
 ?>
